30020cdc49adfdf50804256f175161959a2d8c75
Petra Kallsjö Pullar <petra@behemot.biz>
2024-10-09 06:58:19 +0000

    feat: Crete my-app

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// vanilla JS
+
+// function FunctionalGreeting() {
+//     return <h1>hello from react</h1>
+// }
+
+
+// Arrow function
+
+const FunctionalGreeting = () => <h1>hello from react</h1>;
+
+export default FunctionalGreeting
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// vanilla JS
+
+// function FunctionalGreeting() {
+//     return <h1>hello from react</h1>
+// }
+
+
+// Arrow function
+
+const FunctionalGreetingWithProps = () => <h1>hello</h1>;
+
+export default FunctionalGreetingWithProps
